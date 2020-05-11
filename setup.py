6aa084fded3946fa57a0661b2c8e112c7d8f8192
aaah21 <aaah21@hotmail.com>
2020-05-11 09:36:13 -0400

    Cisco Info Library

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup, find_packages
+import sys
+import os
+
 REQUIRES = ['paramiko']
+CURRENT_PATH = os.path.join(os.path.dirname(__file__))
+sys.path.insert(1, CURRENT_PATH)
+
 setup(
     name='CiscoInfo',
     version='1.0.0',
