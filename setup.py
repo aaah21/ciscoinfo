6e713e143029f0ef4128314dcce115e81740b83e
aaah21 <aaah21@hotmail.com>
2020-05-11 09:27:01 -0400

    Cisco Info Library

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='CiscoInfo',
     version='1.0.0',
-    url='https://github.com/ciscoinfo.git',
+    url='https://github.com/aaah21/ciscoinfo.git',
     author='Aleandro Andrea',
     author_email='aaah1976@gmail.com',
     description='Library to query Cisco devices via ssh',
