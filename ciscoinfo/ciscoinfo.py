5088cdf60d12d21e65430a5f5115fa5b5028a999
aaah21 <aaah21@hotmail.com>
2020-05-11 10:06:18 -0400

    Cisco Info Library

--- ciscoinfo/ciscoinfo.py
+++ ciscoinfo/ciscoinfo.py
@@ -133,7 +133,7 @@ def readfile(filename):
 
 def savefile(fn1, lines_list, headers_list):
     header = ''
-    fn2 = fn1[0:fn1.rfind('.') - 1] + datetime.now().strftime('%Y%m%d_%H%M%S') + fn1[fn1.rfind('.') - 1:]
+    fn2 = fn1[0:fn1.rfind('.')] + datetime.now().strftime('%Y%m%d_%H%M%S') + fn1[fn1.rfind('.') - 1:]
     for i in range(0, len(headers_list)):
         header = header + '"' + headers_list[i] + '"'
         if i != len(headers_list) - 1:
